ae414a3b886c14834e6632652744c83194797056
Spencer Jacklin <sjacklin01@gmail.com>
2023-02-23 09:42:48 -0700

    Setup for Mission 8

---
+++ mission8_group1_3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mission8_group1_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
